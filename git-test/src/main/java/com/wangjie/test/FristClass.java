5a9c49c0f26ef681f8b3f865152bad8ec339d43c
wangjie9587 <75074038@qq.com>
2020-07-29 17:01:59 +0800

    第一个git工程

---
+++ git-test/src/main/java/com/wangjie/test/FristClass.java
@@ -0,0 +1,13 @@
+package com.wangjie.test;
+
+/**
+ * @author 落魄书生
+ * @data 2020/7/29 - 16:59
+ */
+public class FristClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello  ");
+    }
+}
